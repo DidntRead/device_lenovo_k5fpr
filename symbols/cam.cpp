d369790d9a90c5d867b153e9ec79aef8f50dba8c
DidntRead <25854753+DidntRead@users.noreply.github.com>
2019-07-30 14:31:56 +0300

    shim: cam: shim another missing symbol

--- symbols/cam.cpp
+++ symbols/cam.cpp
@@ -3,8 +3,12 @@
 
 extern "C" {
     int _ZN11DpIspStream11startStreamEv();
-  
     int _ZN11DpIspStream11startStreamEP7timeval(struct timeval *endTime) {
         return _ZN11DpIspStream11startStreamEv();
+    }
+    
+    int _ZN11DpIspStream15dequeueFrameEndEv();
+    int _ZN11DpIspStream15dequeueFrameEndEPj(unsigned int* pValueList) {
+        return _ZN11DpIspStream15dequeueFrameEndEv();
     }
 }
